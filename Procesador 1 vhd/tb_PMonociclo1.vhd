04cd2c112a31bcb8aa3534f34f4387deec95ce44
Daniela Zuluaga <daniela-zuluaga@utp.edu.co>
2016-04-17 18:56:37 -0500

    Procesador Monociclo 1 funcional TB

---
+++ Procesador 1 vhd/tb_PMonociclo1.vhd
@@ -0,0 +1,64 @@
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY tb_PMonociclo1 IS
+END tb_PMonociclo1;
+ 
+ARCHITECTURE behavior OF tb_PMonociclo1 IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT PMonociclo1
+    PORT(
+         CLK : IN  std_logic;
+         RESET : IN  std_logic;
+         RESULT : OUT  std_logic_vector(31 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal CLK : std_logic := '0';
+   signal RESET : std_logic := '0';
+
+ 	--Outputs
+   signal RESULT : std_logic_vector(31 downto 0);
+
+   -- Clock period definitions
+   constant CLK_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: PMonociclo1 PORT MAP (
+          CLK => CLK,
+          RESET => RESET,
+          RESULT => RESULT
+        );
+
+   -- Clock process definitions
+   CLK_process :process
+   begin
+		CLK <= '0';
+		wait for CLK_period/2;
+		CLK <= '1';
+		wait for CLK_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+		RESET <='1';
+      wait for 100 ns;
+		RESET <='0';
+		wait;
+   end process;
+
+END;
