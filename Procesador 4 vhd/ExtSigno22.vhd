6428de56842105c34c318d0e615de3480b18bdb9
VanessaPenagos <angie-19-v@utp.edu.co>
2016-05-06 17:41:48 -0500

    Ext de signo para sethi

--- Procesador 4 vhd/ExtSigno22.vhd
+++ Procesador 4 vhd/ExtSigno22.vhd
@@ -9,24 +9,25 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 
 entity ExtSigno is
-    Port ( Dato13 : in  STD_LOGIC_VECTOR (12 downto 0);
+    Port ( Dato22  : in  STD_LOGIC_VECTOR (21 downto 0);
            Dato32 : out  STD_LOGIC_VECTOR (31 downto 0));
 end ExtSigno;
 
 architecture Behavioral of ExtSigno is
 
 begin
-process(Dato13)
+process(Dato22)
 begin
-	if(Dato13(12) = '1') then 
-		Dato32(12 downto 0) <= Dato13;
-		Dato32(31 downto 13) <= (others=>'1');
+	if(Dato13(21) = '1') then 
+		Dato32(21 downto 0) <= Dato22;
+		Dato32(31 downto 22) <= (others=>'1');
 		
 	else 
-		Dato32(12 downto 0) <= Dato13;
-		Dato32(31 downto 13) <= (others=>'0');
+		Dato32(21downto 0) <= Dato22;
+		Dato32(31 downto 22) <= (others=>'0');
 	end if;
 end process;
 
 end Behavioral;
+
 
