ea442c74780f810c270216bb018b4867b7017e27
Daniela Zuluaga <daniela-zuluaga@utp.edu.co>
2016-05-13 16:51:01 -0500

    Extensión de signo 30 to 32

---
+++ Procesador 4 vhd/Extsigno30.vhd
@@ -0,0 +1,32 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- Design Name: 
+-- Module Name:    ExtSigno - Behavioral 
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity ExtSigno is
+    Port ( Dato30  : in  STD_LOGIC_VECTOR (29 downto 0);
+           Dato32 : out  STD_LOGIC_VECTOR (31 downto 0));
+end ExtSigno;
+
+architecture Behavioral of ExtSigno is
+
+begin
+process(Dato30)
+begin
+	if(Dato30(29) = '1') then 
+		Dato32(29 downto 0) <= Dato30;
+		Dato32(31 downto 30) <= (others=>'1');
+		
+	else 
+		Dato32(29 downto 0) <= Dato30;
+		Dato32(31 downto 30) <= (others=>'0');
+	end if;
+end process;
+
+end Behavioral;
+
